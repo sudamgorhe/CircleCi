247832e3916874c1933c07bec3b8f175e84f942f
sudamgorhe <118297381+sudamgorhe@users.noreply.github.com>
2023-05-28 13:21:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 def to_upper(name):
-def to_upper(name):
-    de(me):
-            return name.upper()
+    return name.upper()
 
 
-        def say_hello(name):
-                # Use a breakpoint in the code line below to debug your script.
-                    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def say_hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-                    # Press the green button in the gutter to run the script.
-                    if __name__ == '__main__':
-                            name = 'TrainWithShubham'
-                                say_hello(name)
-                                    up = to_upper(name)
-                                        print(up)to_upper(name):
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    name = 'TrainWithShubham'
+    say_hello(name)
+    up = to_upper(name)
+    print(up)
